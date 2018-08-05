9ffe49ed0ada66c16a1e8322501cfeae030b73d4
Anne Edmondson <edmondson.anne@gmail.com>
2018-08-05 00:57:03 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,9 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  fruit_array = ["mango", "kiwi", "dragonfruit", "cantaloupe"]
+  element = "wow"
+  fruit_array.shift(element)
 end
 
 def retrieve_element_from_index(array, index_number)
