56b96937e13bf56dff07ab4f3f9922987612d06c
Anne Edmondson <edmondson.anne@gmail.com>
2018-08-05 00:56:01 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,9 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  fruit_array = ["mango", "kiwi", "dragonfruit", "cantaloupe"]
+  element = "arrays!"
+  fruit_array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
