d438443b46e6e74f679fcd752c35e3fa1f15677c
Anne Edmondson <edmondson.anne@gmail.com>
2018-08-05 01:06:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  fruit_array = ["mango", "kiwi", "dragonfruit", "cantaloupe"]
+  array = ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
   fruit_array.push(element)
 end
